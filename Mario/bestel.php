935f476c84a276502e398e7645d18c5ab1985890
turbobaap <martinvantongerlo@gmail.com>
2016-09-22 13:59:27 +0200

    tot hfdstk 18
    stukje php en een of ander fancy menu

---
+++ Mario/bestel.php
@@ -0,0 +1,16 @@
+<?php
+	$klant		= $_GET["Naam"];
+	$tel		= $_GET["Telefoonnummer"];
+
+	$aantalmargarita	= $_GET["aantalmargarita"];
+	$aantalhawaii	= $_GET["aantalhawaii"];
+
+echo "beste $klant<br/>										 ";
+echo "Hartelijk dank voor uw bestelling:				<br/>";
+echo "$aantalmargarita 	Pizza Margarita					<br/>";	
+echo "$aantalhawaii 	Pizza Hawaii					<br/>";
+echo "Komt u de bestelling binnen 20 minuten ophalen?  <br/>";
+echo "<br/>";
+echo "Mario";
+
+?>
